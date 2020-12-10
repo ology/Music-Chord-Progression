97c1f02fcc164a15051490d467e3edc0fb2b5a22
Gene Boggs <gene@ology.net>
2020-12-10 09:40:03 -0800

    Not 2. -1

--- lib/Music/Chord/Progression.pm
+++ lib/Music/Chord/Progression.pm
@@ -164,7 +164,7 @@ has octave => (
 Whether to start the progression with the tonic chord or not.
 
 If this is given as C<1> the tonic chord starts the progression.  If
-given as C<0> a neighbor of the tonic is chosen.  If given as C<2> a
+given as C<0> a neighbor of the tonic is chosen.  If given as C<-1> a
 random vertex is chosen.
 
 Default: C<1>
@@ -182,7 +182,7 @@ has tonic => (
 Whether to end the progression with the tonic chord or not.
 
 If this is given as C<1> the tonic chord ends the progression.  If
-given as C<0> a neighbor of the tonic is chosen.  If given as C<2> a
+given as C<0> a neighbor of the tonic is chosen.  If given as C<-1> a
 random vertex is chosen.
 
 Default: C<1>
