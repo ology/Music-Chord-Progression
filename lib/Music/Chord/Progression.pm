79ca38dae1e58efb3e6a261a0b31c7b80d3c7e22
Gene Boggs <gene@ology.net>
2020-12-21 08:34:55 -0800

    Fix resolve == 0 logic

--- lib/Music/Chord/Progression.pm
+++ lib/Music/Chord/Progression.pm
@@ -437,7 +437,7 @@ sub _next_successor {
     }
     elsif ($n == $self->max) {
         if ($self->resolve == 0) {
-            $s = $self->graph->random_successor($n - 1);
+            $s = $self->graph->random_successor($v) || $self->_full_keys;
         }
         elsif ($self->resolve == 1) {
             $s = 1;
