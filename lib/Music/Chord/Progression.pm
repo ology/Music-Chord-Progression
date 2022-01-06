463e1657cd97bb95110a9c39e09c31925f6cf21c
Gene Boggs <gene@ology.net>
2022-01-05 22:32:15 -0800

    Bump version

--- lib/Music/Chord/Progression.pm
+++ lib/Music/Chord/Progression.pm
@@ -2,7 +2,7 @@ package Music::Chord::Progression;
 
 # ABSTRACT: Create network transition chord progressions
 
-our $VERSION = '0.0601';
+our $VERSION = '0.0602';
 
 use Carp qw(croak);
 use Data::Dumper::Compact qw(ddc);
